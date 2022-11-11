6aef0262533cce27c0c41098fd4648a5bdb6cc92
M-IrfanMomand <116905405+M-IrfanMomand@users.noreply.github.com>
2022-11-11 19:53:57 +0430

    Update The main.py
    From Irfan Momand

--- The main.py
+++ The main.py
@@ -199,4 +199,126 @@ while tries_flag != "Close the program" :
 										print("/----------------------Doctor data deleted successfully----------------------/")
 									except :
 										print("Doctor ID should be an integer number")
+										elif Admin_choice == "4" :											#Admin mode --> Doctors Management --> Edit Doctor data
+									try :		#To avoid non integer input	
+										Doctor_ID=input("Enter doctor ID : ")
+										while Doctor_ID not in Doctors_DataBase :
+											Doctor_ID = int(input("Incorrect ID, Please Enter doctor ID : "))
+										print("-----------------------------------------")
+										print("|To Edit doctor's department Enter 1    |")
+										print("|To Edit doctor's name Enter 2          |")
+										print("|To Edit doctor's address Enter 3       |")
+										print("To be Back Enter B                      |")
+										print("-----------------------------------------")
+										Admin_choice=input("Enter your choice : ")
+										Admin_choice = Admin_choice.upper()
+										if Admin_choice == "1" :
+											Doctors_DataBase[Doctor_ID][0][0]=input("Enter Doctor's Department : ")
+											print("/----------------------Doctor's department edited successfully----------------------/")
+											
+										elif Admin_choice == "2" :
+											Doctors_DataBase[Doctor_ID][0][1]=input("Enter Doctor's Name : ")
+											print("----------------------Doctor's name edited successfully----------------------")
+											
+										elif Admin_choice == "3" :
+											Doctors_DataBase[Doctor_ID][0][2]=input("Enter Doctor's Address : ")
+											print("----------------------Doctor's address edited successfully----------------------")
+										
+										elif Admin_choice == "B" :
+											break
+										
+										else :
+											print("\nPlease enter a correct choice\n")
+											
+									except :
+										print("Doctor ID should be an integer number")
+											
+							elif Admin_choice == "B" :											#Back
+										break
+									
+							else :
+								print("\nPlease enter a correct choice\n")
+											
+						elif AdminOptions == "3" :															#Admin mode --> Appointment Management
+							print("-----------------------------------------")
+							print("|To book an appointment Enter 1         |")
+							print("|To edit an appointment Enter 2         |")
+							print("|To cancel an appointment Enter 3       |")
+							print("|To be back enter B                     |")
+							print("-----------------------------------------")
+							Admin_choice = input ("Enter your choice : ")
+							Admin_choice = Admin_choice.upper()
+							if Admin_choice == "1" :												#Admin mode --> Appointment Management --> Book an appointment							
+								try :		#To avoid non integer input
+										Doctor_ID = int(input("Enter the ID of doctor : "))
+										while Doctor_ID not in Doctors_DataBase :
+											Doctor_ID = int(input("Doctor ID incorrect, Please enter a correct doctor ID : "))
+										print("---------------------------------------------------------")
+										print("|For book an appointment for an exist patient Enter 1   |\n|For book an appointment for a new patient Enter 2      |\n|To be Back Enter B                                     |")
+										print("---------------------------------------------------------")
+										Admin_choice = input ("Enter your choice : ")
+										Admin_choice = Admin_choice.upper()
+										if Admin_choice == "1" :
+												patient_ID = int(input("Enter patient ID : "))
+												while patient_ID not in Pateints_DataBase :		#if Admin entered incorrect ID
+													patient_ID = int(input("Incorrect ID, please Enter a correct patient ID : "))	
+										
+											
+										elif Admin_choice == "2" :
+											patient_ID = int(input("Enter patient ID : "))
+											while patient_ID in Pateints_DataBase :		#if Admin entered used ID
+												patient_ID = int(input("This ID is unavailable, please try another ID : "))					
+											Department=Doctors_DataBase[Doctor_ID][0][0]
+											DoctorName=Doctors_DataBase[Doctor_ID][0][1]
+											Name      =input("Enter patient name    : ")
+											Age       =input("Enter patient age     : ")
+											Gender    =input("Enter patient gender  : ")
+											Address   =input("Enter patient address : ")
+											RoomNumber=""
+											Pateints_DataBase[patient_ID]=[Department,DoctorName,Name,Age,Gender,Address,RoomNumber]
+										
+										elif Admin_choice == "B" :
+											break
+											
+										Session_Start = input("Session starts at : ")
+										while Session_Start[ :2] == "11" or Session_Start[ :2] == "12" :
+											Session_Start = input("Appointments should be between 01:00PM to 10:00PM, Please enter a time between working hours : ")
+											
+										for i in Doctors_DataBase[Doctor_ID] :
+											if type(i[0])!=str :
+												while Session_Start >= i[1] and Session_Start < i[2] :
+													Session_Start = input("This appointment is already booked, Please Enter an other time for start of session : ")
+										Session_End   = input("Session ends at : ")
+										
+										New_Appointment=list()
+										New_Appointment.append(patient_ID)
+										New_Appointment.append(Session_Start)
+										New_Appointment.append(Session_End)
+										Doctors_DataBase[Doctor_ID].append(New_Appointment)								
+										print("/----------------------Appointment booked successfully----------------------/")
+								except :
+										print("Doctor ID should be an integer number")
+					
+							elif Admin_choice == "2" :												#Admin mode --> Appointment Management --> Edit an appointment
+									try :		#To avoid non integer input
+										patient_ID = int(input("Enter patient ID : "))						
+										while patient_ID not in Pateints_DataBase :
+											patient_ID = int(input("Incorrect Id, Please Enter correct patient ID : "))
+										try :   #To avoid no return function
+												AppointmentIndex,PairKey = AppointmentIndexInDoctorsDataBase(patient_ID)
+												Session_Start = input ("Please enter the new start time : ")
+												while Session_Start[ :2] == "11" or Session_Start[ :2] == "12" :
+													Session_Start = input("Appointments should be between 01:00PM to 10:00PM, Please enter a time between working hours : ")
+													
+												for i in Doctors_DataBase[Doctor_ID] :
+													if type(i[0])!=str :
+														while Session_Start >= i[1] and Session_Start < i[2] :
+															Session_Start = input("This appointment is already booked, Please Enter an other time for start of session : ")
+												Session_End = input ("Please enter the new end time : ")
+												Doctors_DataBase[PairKey][AppointmentIndex]=[patient_ID,Session_Start,Session_End]							
+												print("/----------------------appointment edited successfully----------------------/")
+										except :
+												print("No Appointment for this patient")
+									except :
+										print("Doctor ID should be an integer number")
 											
